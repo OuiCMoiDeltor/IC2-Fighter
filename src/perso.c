34e0e2c7ade61cdf6bc909943e9cea1221aa1eb3
GHRIB YACINE <yass72k@gmail.com>
2024-04-16 00:04:16 +0200

    suite dev perso

--- src/perso.c
+++ src/perso.c
@@ -2,6 +2,8 @@
 #include "../lib/creation.h"
 #include "../lib/perso.h"
 #include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
+
 
 #define VIE_MAX 100
 #define DMG 5
@@ -202,9 +204,9 @@ personnage *creerPerso(SDL_Renderer *renderer, char *image, int *largeur, int *h
     perso->hp->pv = VIE_MAX;
     perso->hp->barrePv = malloc(sizeof(SDL_Rect));
     perso->hp->barrePv->x = 100/8;
-    perso->hp->barrePv->y = 100/8;
-    perso->hp->barrePv->w = 400;
-    perso->hp->barrePv->h = 60;
+    perso->hp->barrePv->y = 100/2;
+    perso->hp->barrePv->w = 550;
+    perso->hp->barrePv->h = 35;
     
     return perso;
 }
@@ -222,19 +224,58 @@ personnage *creerPerso(SDL_Renderer *renderer, char *image, int *largeur, int *h
 */
 
 extern
-void mettreAJourHp(SDL_Renderer *renderer, personnage * perso1, int degat) {
-    Mix_Chunk *soundDMG = Mix_LoadWAV("mixer/ouh.wav") ;
-    SDL_Log("%d", perso1->hp->pv); // Affichage des pv restants dans la console pour test
-    perso1->hp->pv = perso1->hp->pv - degat < 0 ? 0 : perso1->hp->pv - degat;  // Assure une réduction correcte de la vie
-    SDL_Delay(100) ;
-    // Mise à jour de la largeur de la barre de vie selon la vie actuelle
-    perso1->hp->barrePv->w = 400 * perso1->hp->pv / VIE_MAX;
+void mettreAJourHp(SDL_Renderer *renderer, personnage * perso, int degat, int persoID) {
+    Mix_Chunk *soundDMG = Mix_LoadWAV("mixer/ouh.wav");
+    Mix_VolumeChunk(soundDMG, MIX_MAX_VOLUME / 3);
+    SDL_Log("%d", perso->hp->pv);  // Affiche les points de vie restants pour le test
+    perso->hp->pv = perso->hp->pv - degat < 0 ? 0 : perso->hp->pv - degat;  // Réduit la vie sans descendre en dessous de 0
+
+    // Calcule la nouvelle largeur de la barre de vie en fonction des points de vie actuels
+    int newWidth = 550 * perso->hp->pv / VIE_MAX;
+
+    // Comportement inversé (réduction de droite à gauche) pour perso2
+    if (persoID == 1) {
+        int deltaX = perso->hp->barrePv->w - newWidth;
+        perso->hp->barrePv->x += deltaX;
+        perso->hp->barrePv->w = newWidth;
+    } else {
+        
+        perso->hp->barrePv->w = newWidth;  
+    }
+
     Mix_PlayChannel(-1, soundDMG, 0);
+
+    SDL_Delay(100);
 }
+
+
+
+
 
 extern
 void mettreAJourPersonnage(SDL_Renderer *renderer, personnage *perso1, personnage *perso2, const Uint8 *keyboardState, int largeur, int hauteur) {
+    /*Test avant que Quentin commence à crier, fonctionne mais comme ttf charge en boucle lors d'appel -> jeu tres ralenti*/
+    /*
+    TTF_Font *font = TTF_OpenFont("ttf/Act_Of_Rejection.ttf", 32);
+    SDL_Color textColor = {255, 255, 255, 255}; 
+    SDL_Surface *surfaceJ1 = TTF_RenderText_Solid(font, "CACAPAGNAN", textColor);
+    SDL_Texture *textureJ1 = SDL_CreateTextureFromSurface(renderer, surfaceJ1);
+    SDL_Rect textRectJ1 = {100/8, 100/16, surfaceJ1->w, surfaceJ1->h};
+
+    SDL_RenderCopy(renderer, textureJ1, NULL, &textRectJ1);
+    SDL_DestroyTexture(textureJ1);
+    SDL_FreeSurface(surfaceJ1);
+
+    SDL_Surface *surface = TTF_RenderText_Solid(font, "easy", textColor);
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect textRect = {720, 100/16, surface->w, surface->h};
+
+    SDL_RenderCopy(renderer, texture, NULL, &textRect);
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(surface);
+    */
     Mix_Chunk *soundHIT = Mix_LoadWAV("mixer/hit.wav") ;
+    Mix_VolumeChunk(soundHIT, MIX_MAX_VOLUME / 8);
     // Gestion dégats J1
     if (keyboardState[SDL_SCANCODE_P]) {
         perso1->etatAnimation = 0;
@@ -242,16 +283,16 @@ void mettreAJourPersonnage(SDL_Renderer *renderer, personnage *perso1, personnag
         if(perso1->blocking) {
             perso1->etatAnimation = 1;
             perso1->animation = PARADE;
-            mettreAJourHp(renderer, perso1, DMG / 2) ;
+            mettreAJourHp(renderer, perso1, DMG / 2, 1) ;
         }else if(perso1->animation == SAUT) {
             perso1->animation = DEGATSAUT;
-            mettreAJourHp(renderer, perso1, DMG) ;
+            mettreAJourHp(renderer, perso1, DMG, 1) ;
         }else if(perso1->crouching) {
             perso1->animation = DEGATACCROUPI;
-            mettreAJourHp(renderer, perso1, DMG) ; 
+            mettreAJourHp(renderer, perso1, DMG, 1) ; 
         }else {
             perso1->animation = DEGAT;
-            mettreAJourHp(renderer, perso1, DMG) ;
+            mettreAJourHp(renderer, perso1, DMG, 1) ;
         }
     }
 
@@ -273,6 +314,10 @@ void mettreAJourPersonnage(SDL_Renderer *renderer, personnage *perso1, personnag
     fprintf(stderr, "Plus de vie. \n");
     }
 
+    // Positionnement barre de vie perso2
+    perso2->hp->barrePv->x = 720;
+    perso2->hp->barrePv->y = 100 / 2;
+
     // Gestion dégats J2
     if (keyboardState[SDL_SCANCODE_O]) {
         perso2->etatAnimation = 0;
@@ -280,17 +325,35 @@ void mettreAJourPersonnage(SDL_Renderer *renderer, personnage *perso1, personnag
         if(perso2->blocking) {
             perso2->etatAnimation = 1;
             perso2->animation = PARADE;
-            mettreAJourHp(renderer, perso2, DMG / 2) ;
+            mettreAJourHp(renderer, perso2, DMG / 2, 2) ;
         }else if(perso2->animation == SAUT) {
             perso2->animation = DEGATSAUT;
-            mettreAJourHp(renderer, perso2, DMG) ;
+            mettreAJourHp(renderer, perso2, DMG, 2) ;
         }else if(perso2->crouching) {
             perso2->animation = DEGATACCROUPI;
-            mettreAJourHp(renderer, perso2, DMG) ;
+            mettreAJourHp(renderer, perso2, DMG, 2) ;
         }else {
             perso2->animation = DEGAT;
-            mettreAJourHp(renderer, perso2, DMG) ;
+            mettreAJourHp(renderer, perso2, DMG, 2) ;
         }
+    }
+
+    //Barre de vie à états, changement de sa couleur en fonction des points de vie restants
+    if (perso2->hp->pv > 80) {
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);  // Vert
+    } else if (perso2->hp->pv > 60) {
+        SDL_SetRenderDrawColor(renderer, 144, 238, 144, 255);  // Vert clair
+    } else if (perso2->hp->pv > 40) {
+        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);  // Jaune
+    } else if (perso2->hp->pv > 20) {
+        SDL_SetRenderDrawColor(renderer, 255, 165, 0, 255);  // Orange
+    } else {
+        SDL_SetRenderDrawColor(renderer, 220, 20, 60, 255);  // Rouge
+    }
+    SDL_RenderFillRect(renderer, perso2->hp->barrePv);
+    //Condition à compléter lorsque écran rematch fait
+    if (perso2->hp->pv <= 0) {
+    fprintf(stderr, "Plus de vie. \n");
     }
 
     // perso1
